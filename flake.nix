93616dd188c73644e1dd04283e8a719993cc6d8d
YisuiDenghua <102890144+YisuiDenghua@users.noreply.github.com>
2026-03-26 10:18:28 +0800

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Flatrun: Run Flatpak apps by name";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.callPackage ./nix/default.nix { version = "0.1.0"; };
+
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/flatrun";
+        };
+      }
+    );
+}
