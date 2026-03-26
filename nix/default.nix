b93738ff29898cad0c931b2a225e9edc3103ec74
YisuiDenghua <102890144+YisuiDenghua@users.noreply.github.com>
2026-03-26 10:23:20 +0800

    Update default.nix

--- nix/default.nix
+++ nix/default.nix
@@ -1,3 +1,49 @@
-{ pkgs ? import <nixpkgs> { } }:
+{
+  pkgs,
+  lib,
+  version ? "0.1.0",
+}:
 
-(pkgs.callPackage ./package.nix { })
+pkgs.stdenv.mkDerivation {
+  pname = "flatrun";
+  inherit version;
+
+  src = pkgs.fetchzip {
+    url = "https://github.com/YisuiDenghua/flatrun/archive/refs/tags/v${version}.tar.gz";
+    sha256 = "sha256-+YUQ7ZKM3NVLoP1G7hvgJX11rguHLcQniO93IuyGltg=";
+  };
+
+  nativeBuildInputs = [ pkgs.makeWrapper ];
+
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm755 flatrun.sh $out/bin/flatrun
+    # link to "fr"
+    ln -s $out/bin/flatrun $out/bin/fr
+
+    runHook postInstall
+  '';
+
+  # run time wrap
+  postFixup = ''
+    wrapProgram $out/bin/flatrun \
+      --prefix PATH : ${
+        lib.makeBinPath [
+          pkgs.flatpak
+          pkgs.gawk
+          pkgs.gnused
+          pkgs.coreutils
+        ]
+      }
+  '';
+
+  meta = with lib; {
+    description = "A simple wrapper to run Flatpak apps by matching name or ID";
+    homepage = "https://github.com/YisuiDenghua/flatrun";
+    license = licenses.mit; 
+    platforms = platforms.linux;
+    maintainers = with lib.maintainers; [ yisuidenghua ];
+    mainProgram = "flatrun";
+  };
+}
